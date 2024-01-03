45f557f334fddc6a7bbdd967e0588d2fb9679d30
Jasper Wang <jasper8777@icloud.com>
2024-01-03 14:42:02 -0800

    implement TOC and add markdown stylings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+// import remarkToc from "remark-toc";
 
 export default defineConfig({
-  site: 'https://www.jasperwang.dev/',
+  site: "https://www.jasperwang.dev/",
+  markdown: {
+    // remarkPlugins: [[remarkToc, { heading: "contents" }]],
+    shikiConfig: {
+      theme: "nord",
+      // Enable word wrap to prevent horizontal scrolling
+      wrap: true,
+    },
+  },
 });
