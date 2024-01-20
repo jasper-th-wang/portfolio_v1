3f61c810ece12cf730ed8073280cbed931f73b00
Jasper Wang <jasper8777@icloud.com>
2024-01-20 13:53:43 -0800

    install react and tailwind, fix tailwind preflight styling affecting existing css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 // import remarkToc from "remark-toc";
+import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
 export default defineConfig({
   site: "https://www.jasperwang.dev/",
   markdown: {
@@ -8,7 +12,8 @@ export default defineConfig({
     shikiConfig: {
       theme: "nord",
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true,
-    },
+      wrap: true
+    }
   },
+  integrations: [react(), tailwind()]
 });
