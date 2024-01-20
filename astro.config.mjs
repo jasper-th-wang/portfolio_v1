d2755e2c2321dd597ce9b2fbf040366612ec72e2
Jasper Wang <jasper8777@icloud.com>
2024-01-20 15:00:34 -0800

    fix tailwind base styling issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,24 @@
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 // import remarkToc from "remark-toc";
-import react from "@astrojs/react";
+import react from '@astrojs/react';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.jasperwang.dev/",
+  site: 'https://www.jasperwang.dev/',
   markdown: {
     // remarkPlugins: [[remarkToc, { heading: "contents" }]],
     shikiConfig: {
-      theme: "nord",
+      theme: 'nord',
       // Enable word wrap to prevent horizontal scrolling
-      wrap: true
-    }
+      wrap: true,
+    },
   },
-  integrations: [react(), tailwind()]
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
