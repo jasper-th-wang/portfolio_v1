b7ce22107f81315d49cba03798876eb2fc9b7173
Jasper Wang <jasper8777@icloud.com>
2026-03-25 23:37:07 -0700

    fix: link issues

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -13,6 +13,9 @@ export const ui = {
     'nav.experience': 'experience',
     'nav.projects': 'projects',
     'nav.posts': 'posts',
+    'btn.viewExperience': 'View more experience',
+    'btn.viewProjects': 'View more projects',
+    'btn.viewPosts': 'View more posts:'
   },
   zh: {
     'header.title': '軟體工程師',
@@ -21,5 +24,8 @@ export const ui = {
     'nav.experience': '經歷',
     'nav.projects': '專案',
     'nav.posts': '文章',
+    'btn.viewExperience': '查看更多經歷',
+    'btn.viewProjects': '查看更多專案',
+    'btn.viewPosts': '查看更多文章'
   },
 } as const;
