73167b794eacfe877596ba68e41253b100c59962
Jasper Wang <jasper8777@icloud.com>
2026-03-25 22:49:13 -0700

    feat: add localization for home page

---
+++ src/i18n/ui.ts
@@ -0,0 +1,25 @@
+export const languages = {
+  en: 'English',
+  zh: '中文',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'header.title': 'Software Developer',
+    'nav.about': 'about',
+    'nav.education': 'education',
+    'nav.experience': 'experience',
+    'nav.projects': 'projects',
+    'nav.posts': 'posts',
+  },
+  zh: {
+    'header.title': '軟體工程師',
+    'nav.about': '關於我',
+    'nav.education': '學歷',
+    'nav.experience': '經歷',
+    'nav.projects': '專案',
+    'nav.posts': '文章',
+  },
+} as const;
